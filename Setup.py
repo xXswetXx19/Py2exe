dac3a262a117a7b66d309b8847bc49cc025cf63a
xXswetXx19 <samuelsebastiangonzalez@gmail.com>
2022-11-16 22:26:07 -0500

    first commit

---
+++ Setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+import py2exe
+
+Archivos = [
+    ('Archivos/imgs/', ['Archivos/imgs/logo.png', 'Archivos/imgs/Icono.ico', 'Archivos/imgs/Registro.png']),
+    ('Clibs', ['Clibs/Autocomplete.py']),
+    ('', ['Registro.db'])
+    ]
+
+setup(
+    options = 
+    {'py2exe': {
+        "packages": [], 
+        'bundle_files': 3, 
+        'compressed': True,
+        'includes': []
+        }},
+    data_files = Archivos,
+    windows = [{
+            "script":"Registrar.py",
+            "icon_resources": [(1, "Archivos/imgs/Icono.ico")],
+            "dest_base":"Registrar Sumliprob"
+            }],
+    package_dir = {'Packages': ''},
+
+
+)
